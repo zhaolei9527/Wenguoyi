b56e636e1fcbd076a4324242ab561094b0c784ef
sakura <975976959@qq.com>
2018-05-25 15:47:32 +0800

    201805231547

---
+++ app/src/main/java/com/wenguoyi/Activity/YiShengDetailsActivity.java
@@ -0,0 +1,34 @@
+package com.wenguoyi.Activity;
+
+import com.wenguoyi.Base.BaseActivity;
+import com.wenguoyi.R;
+
+/**
+ * com.wenguoyi.Activity
+ *
+ * @author 赵磊
+ * @date 2018/5/25
+ * 功能描述：
+ */
+public class YiShengDetailsActivity extends BaseActivity {
+
+    @Override
+    protected int setthislayout() {
+        return R.layout.yishengdetails_activity_layout;
+    }
+
+    @Override
+    protected void initview() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
--- app/src/main/java/com/wenguoyi/Activity/YiShengListActivity.java
+++ app/src/main/java/com/wenguoyi/Activity/YiShengListActivity.java
@@ -46,6 +46,7 @@ public class YiShengListActivity extends BaseActivity implements View.OnClickLis
     private int p = 1;
     private WenguoyiRecycleView rv_yisheng_list;
     private SakuraLinearLayoutManager line;
+    private WindowManager.LayoutParams lp;
 
     @Override
     protected int setthislayout() {
@@ -75,7 +76,7 @@ public class YiShengListActivity extends BaseActivity implements View.OnClickLis
         rv_yisheng_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                startActivity(new Intent(context, PriceDetailsActivity.class));
+                startActivity(new Intent(context, YiShengDetailsActivity.class));
             }
         });
     }
@@ -88,8 +89,8 @@ public class YiShengListActivity extends BaseActivity implements View.OnClickLis
     protected void initListener() {
         llKeshi.setOnClickListener(this);
         llZhicheng.setOnClickListener(this);
-        initPopupWindow();
-
+        initKeShiPopupWindow();
+        initZhiChengPopupWindow();
     }
 
     @Override
@@ -110,33 +111,41 @@ public class YiShengListActivity extends BaseActivity implements View.OnClickLis
         //win.setAnimationStyle(R.style.animTranslate);
         switch (view.getId()) {
             case R.id.ll_keshi:
-                window.showAsDropDown(llKeshi, 0, 0);
-                WindowManager.LayoutParams lp = getWindow().getAttributes();
+                keshiwindow.showAsDropDown(llKeshi, 0, 0);
+                lp = getWindow().getAttributes();
                 lp.alpha = 0.3f;
                 getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
                 getWindow().setAttributes(lp);
                 break;
             case R.id.ll_zhicheng:
+                zhichengwindow.showAsDropDown(llZhicheng, 0, 0);
+                lp = getWindow().getAttributes();
+                lp.alpha = 0.3f;
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+                getWindow().setAttributes(lp);
                 break;
             default:
                 break;
         }
     }
 
-    private CommonPopupWindow window;
+    private CommonPopupWindow keshiwindow;
+    private CommonPopupWindow zhichengwindow;
+
     private FlowLayout mFlowLayout;
-    private String[] mVals = new String[]{"苹果手机", "笔记本电脑笔记本电脑", "电饭煲 ", "腊肉",
-            "特产", "剃须刀", "宝宝", "康佳"};
+
+    private String[] mkeshiVals = new String[]{"全部科室", "妇产科", "儿科 ", "综合门诊",
+            "内科", "外壳", "中医科", "皮肤性病科", "康复医学", "重症医学科", "其他科室"};
     private LayoutInflater mInflater;
 
-    private void initPopupWindow() {
+    private void initKeShiPopupWindow() {
         mInflater = LayoutInflater.from(this);
         // get the height of screen
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
         int screenHeight = metrics.heightPixels;
         // create popup window
-        window = new CommonPopupWindow(this, R.layout.yisheng_keshi_popupwindow_layout, ViewGroup.LayoutParams.MATCH_PARENT, screenHeight) {
+        keshiwindow = new CommonPopupWindow(this, R.layout.yisheng_keshi_popupwindow_layout, ViewGroup.LayoutParams.MATCH_PARENT, screenHeight) {
             @Override
             protected void initView() {
                 View view = getContentView();
@@ -144,10 +153,10 @@ public class YiShengListActivity extends BaseActivity implements View.OnClickLis
                 /**
                  * 找到搜索标签的控件
                  */
-                for (int i = 0; i < mVals.length; i++) {
+                for (int i = 0; i < mkeshiVals.length; i++) {
                     TextView tv = (TextView) mInflater.inflate(
-                            R.layout.label_tv_layout, mFlowLayout, false);
-                    tv.setText(mVals[i]);
+                            R.layout.label_tv_keshi_layout, mFlowLayout, false);
+                    tv.setText(mkeshiVals[i]);
                     final String str = tv.getText().toString();
                     //点击事件
                     tv.setOnClickListener(new View.OnClickListener() {
@@ -157,6 +166,62 @@ public class YiShengListActivity extends BaseActivity implements View.OnClickLis
                         }
                     });
                     mFlowLayout.addView(tv);//添加到父View
+                }
+
+            }
+
+            @Override
+            protected void initEvent() {
+
+            }
+
+            @Override
+            protected void initWindow() {
+                super.initWindow();
+                PopupWindow instance = getPopupWindow();
+                instance.setOnDismissListener(new PopupWindow.OnDismissListener() {
+                    @Override
+                    public void onDismiss() {
+                        WindowManager.LayoutParams lp = getWindow().getAttributes();
+                        lp.alpha = 1.0f;
+                        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+                        getWindow().setAttributes(lp);
+                    }
+                });
+            }
+        };
+    }
+
+
+    private void initZhiChengPopupWindow() {
+        mInflater = LayoutInflater.from(this);
+        // get the height of screen
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        int screenHeight = metrics.heightPixels;
+        // create popup window
+        zhichengwindow = new CommonPopupWindow(this, R.layout.yisheng_zhicheng_popupwindow_layout, ViewGroup.LayoutParams.MATCH_PARENT, screenHeight) {
+            @Override
+            protected void initView() {
+                View view = getContentView();
+                LinearLayout ll_zhicheng = (LinearLayout) view.findViewById(R.id.ll_zhicheng);
+                /**
+                 * 找到搜索标签的控件
+                 */
+                for (int i = 0; i < mkeshiVals.length; i++) {
+                    LinearLayout label_tv_zhicheng_layout = (LinearLayout) mInflater.inflate(
+                            R.layout.label_tv_zhicheng_layout, mFlowLayout, false);
+                    TextView tv = label_tv_zhicheng_layout.findViewById(R.id.tv_zhicheng);
+                    tv.setText(mkeshiVals[i]);
+                    final String str = tv.getText().toString();
+                    //点击事件
+                    label_tv_zhicheng_layout.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+
+                        }
+                    });
+                    ll_zhicheng.addView(label_tv_zhicheng_layout);//添加到父View
                 }
 
             }
